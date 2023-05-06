421beed17beb88a58fba9f0544f4e5607e0c9401
chiggsaroni <saetern.chanh@gmail.com>
2023-05-06 15:09:10 -0700

    added hover, and button fuctionality

--- script.js
+++ script.js
@@ -1,4 +1,61 @@
+let color = "black";
 
+document.addEventListener("DOMContentLoaded", function () {
+    createBoard(16);
 
+    let btn_popup = document.querySelector("#popup");
+    btn_popup.addEventListener("click", function(){
+       let size = getSize();
+       createBoard(size);
+    })
+})
 
+function createBoard(size) {
+    let board = document.querySelector(".container");
+
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let grid = size * size;
+
+    for (i = 0; i < grid; i++) {
+        let square = document.createElement("square");
+        square.addEventListener("mouseover", colorDiv);
+        board.insertAdjacentElement("beforeend", square);
+    }
+}
+
+function getSize() {
+    let choice = prompt("Enter a number");
+    let message = document.querySelector("#message")
+    if(choice == ""){
+        message.innerHTML = "Please provide a number";
+    }
+    else if(choice < 0 || choice >100) {
+        message.innerHTML =  "Please provide a number between 1-100";
+    }
+    else {
+        message.innerHTML = "Done!";
+        return choice;
+
+    }
+}
+
+function colorDiv(){
+    if (color == "random") {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    }
+    else {
+       this.style.backgroundColor = "black" 
+    }
+}
+
+function setColor (colorChoice){
+    color = colorChoice;
+}
+
+function resetBoard() {
+    let clear =  document.querySelectorAll("square");
+    clear.forEach((div) => div.style.backgroundColor = "white")
+}
 
